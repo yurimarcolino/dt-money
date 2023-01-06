38c8e1c243c8b3950b8c58f81435182527e9bdd9
yurimarcolino <yurifmarcolino@gmail.com>
2023-01-06 11:42:47 -0300

    feat: adding search form component

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,14 @@
+import { MagnifyingGlass } from 'phosphor-react'
+import { SearchFormContainer } from './styles'
+
+export function SeachForm() {
+  return (
+    <SearchFormContainer>
+      <input type="text" placeholder="Busque por transações" />
+      <button type="submit">
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </SearchFormContainer>
+  )
+}
